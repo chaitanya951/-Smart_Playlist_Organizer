11bd855e7a8e008cb509ada4ea1b906c42c2f86e
chaitanya951 <155001701+chaitanya951@users.noreply.github.com>
2025-07-04 13:49:54 +0530

    Update background.js
    Added with Comments

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
+// Listen for updates on any tab
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url && tab.url.includes('youtube.com')) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ['content_script.js']
-      });
-    }
-  });
-  
+
+  // Check if the URL changed and it's a YouTube URL
+  if (changeInfo.url && tab.url.includes('youtube.com')) {
+    
+    // Inject the content script into the updated YouTube tab
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['content_script.js']
+    });
+  }
+});
